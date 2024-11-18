96f55292ed8219c68c396f45a0688ea2dc545c82
ebata-rafael <ebata.rafael@ufms.br>
2024-11-17 21:49:35 -0300

    serviço para cadastrar um usuário novo

--- src/app/layout-login/cadastro/cadastro.component.ts
+++ src/app/layout-login/cadastro/cadastro.component.ts
@@ -1,15 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../service/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrl: './cadastro.component.scss',
 })
-
 export class CadastroComponent {
-  @Input() x: string = '';
-}
+  user = { name: '', email: '', password: '' };
+  confirmPassword = '';
+  message = ''; // Propriedade para exibir mensagem de confirmação
+  constructor(private userService: UserService) {}
+  register() {
+    if (this.user.password === this.confirmPassword) {
+      this.userService.createUser(this.user).subscribe(
+        (response) => {
+          console.log('Usuário cadastrado com sucesso!', response);
+          this.message = 'Usuário cadastrado com sucesso!'; // Define a mensagem de sucesso this.resetForm(); // Reseta o formulário
+          this.resetForm(); // Reseta o formulário
+        },
+        (error) => {
+          console.error('Erro ao cadastrar usuário', error);
+        }
+      );
+    } else {
+      console.error('Senhas não coincidem.');
+    }
+  }
 
+  resetForm() {
+    this.user = { name: '', email: '', password: '' };
+    this.confirmPassword = '';
+  }
+}
