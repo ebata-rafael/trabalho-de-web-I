1716e1276c3eaa1a75603734ae674dbb2be9bbe5
Makakof <luiz.h.s.silva@ufms.br>
2024-11-23 19:34:58 -0300

    "Mudanças feitas em sala com Humberto"

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,29 +2,36 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RecipesService } from '../service/recipes.service';
 import { RecipeResponse } from '../models/recipe.model';
 import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PaginationModule],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.scss'
 })
 export class RecipesComponent implements OnInit{
 
   private readonly recipeService = inject(RecipesService);
-
-
-  recipes: RecipeResponse = { meta: { itemCount: 0, totalItems: 0, itemsPerPage: 0, totalPages: 0, currentPage: 0 }, items: [] };
+  recipes!: RecipeResponse;
+  currentPage: number = 1;
 
   ngOnInit(): void {
-    this.recipeService.list().subscribe((result) => {
+    this.recipeService.list(1,8).subscribe((result) => {
       this.recipes = result;
+      this.currentPage = this.recipes.meta.currentPage
     });
-
   }
 
   getStars(score: number): Array<number> {
     return Array(score).fill(0);
+  }
+
+  pageChanged(event:any) {
+    this.recipeService.list(event.page, event.itemsPerPage).subscribe((result) => {
+      this.recipes = result;
+      this.currentPage = this.recipes.meta.currentPage
+    });
   }
 }
